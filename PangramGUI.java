bf6c1b887d6c8faacb895f1c79085c518d1ed7c3
Dmitri Stanchevici <dmitristanchevici@Dmitris-iMac.local>
2023-01-11 14:13:56 -0500

    added PangramGUI

---
+++ PangramGUI.java
@@ -0,0 +1,119 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class PangramGUI extends JFrame {
+
+    JTextArea textArea;
+    JTextField stringField;
+    JScrollPane scrollPane;
+
+    public PangramGUI ()
+    {
+        // Set some parameters of the frame (window) that's brought up.
+        this.setSize (800, 400);
+        this.setTitle ("Pangram Tester");
+        this.setResizable (true);
+
+        // This is how stuff is put into a frame.
+	Container cPane = this.getContentPane();
+        textArea = new JTextArea ();
+	scrollPane = new JScrollPane (textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        cPane.add (scrollPane, BorderLayout.CENTER);
+        
+        // Make the controls.
+        JPanel panel = new JPanel ();
+        JLabel label = new JLabel ("Enter a string: ");
+        panel.add (label);
+        stringField = new JTextField (30);
+        panel.add (stringField);
+        JButton button = new JButton ("Go");
+	button.addActionListener (
+	  new ActionListener () {
+	      public void actionPerformed (ActionEvent a)
+	      {
+		  handleButtonClick();
+	      }
+	  }
+        );
+        panel.add (button);
+
+        button = new JButton ("Quit");
+	button.addActionListener (
+	  new ActionListener () {
+	      public void actionPerformed (ActionEvent a)
+	      {
+		  System.exit(0);
+	      }
+	  }
+        );
+        panel.add (button);
+
+        cPane.add (panel, BorderLayout.SOUTH);
+        
+        this.setVisible (true);
+    }
+    
+
+    String inputStr;
+
+
+    // When the user clicks the button, this method gets called.
+    // It's where we need to respond.
+
+    void handleButtonClick ()
+    {
+        // Extract the string from the textfield where the user typed the strings.
+        inputStr = stringField.getText ();
+
+	String outputStr = textArea.getText();
+	if ( checkPangram(inputStr) ) {
+	    outputStr += inputStr + " is a pangram\n\n";
+	}
+	else {
+	    outputStr += inputStr + " is NOT a pangram\n\n";
+	}
+
+        // Put the output string in the text box.
+	textArea.setText (outputStr);
+    }
+
+
+    static boolean checkPangram (String str)
+    {
+        int start = (int) 'a';
+        int end = (int) 'z';
+
+	char[] letters = str.toCharArray ();
+
+	for (int i=start; i<=end; i++) {
+
+	    boolean charFound = false;
+
+	    for (int j=0; j < letters.length; j++) {
+		int nOfCh = (int) letters[j];
+		if (i == nOfCh) {
+		    charFound = true;
+		    break;
+		}
+	    }
+	    
+	    if (charFound == false) {
+		return false;
+	    }
+
+	}
+
+	return true;
+
+    }
+
+
+    public static void main (String[] argv)
+    {
+	PangramGUI a = new PangramGUI ();
+    }
+
+}
